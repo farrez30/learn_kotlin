b5864608501b3feb4e7cf6c249b06366c26d0315
farrez30 <59773413+farrez30@users.noreply.github.com>
2025-01-21 16:17:24 +0700

    to String

---
+++ src/main/kotlin/com/company2/oop/app/14_toString.kt
@@ -0,0 +1,11 @@
+package com.company2.oop.app
+
+import com.company2.oop.data.User
+
+fun main() {
+    val user1 = User("Ayam", "rahasia");
+    val user2 = User("Bebek", "rahasia2");
+
+    println(user1)
+    println(user2)
+}
--- src/main/kotlin/com/company2/oop/data/User.kt
+++ src/main/kotlin/com/company2/oop/data/User.kt
@@ -3,4 +3,7 @@ package com.company2.oop.data
 // Properties di Constructor langsung
 // Cuma bisa di primary constructor, tidak bisa di secondary constructor.
 class User(var username: String, var password: String) {
+    override fun toString(): String {
+        return "User with username=$username"
+    }
 }
